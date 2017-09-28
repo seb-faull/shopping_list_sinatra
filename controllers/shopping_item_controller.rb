0fa0ff7a407e019ebadfac490c4c265caf60357e
seb-faull <seb_faull@hotmail.co.uk>
2017-09-28 19:15:18 +0100

    Added placeholder shopping item's body and titles

--- controllers/shopping_item_controller.rb
+++ controllers/shopping_item_controller.rb
@@ -4,6 +4,23 @@ class ShoppingItemsController < Sinatra::Base
 		register Sinatra::Reloader
 	end
 
+	set :root, File.join(File.dirname(__FILE__), "..")
+
+	set :views, Proc.new { File.join(root, 'views') }
+
+
+	$shopping_items = [{
+		title: "Item 1",
+		body: "This is the first shopping item"
+	},
+	{
+		title: "Item 2"
+		body: "This is the second shopping item"
+	},
+	{
+		title: "Item 3"
+		body: "This is the third shopping item"
+	}]
 
 
 
